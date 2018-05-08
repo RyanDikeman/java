f8c36e15094f1a609ed5556245f6aa8def4d40cd
Ryan4245 <31550030+RyanDikeman@users.noreply.github.com>
2018-05-08 20:02:59 +0000

    Add files via upload

---
+++ RandomDouble.java
@@ -0,0 +1,22 @@
+import java.until.*;
+class RandomDouble {
+	public static void main (String args[])throws InterruptedException{
+		double value = 0;
+		int i; // for loop counter
+		int max = 10; // maximum number of cards or random values
+		 int count = 0;
+		 double firstmin = -1.0;
+		 System.out.println("*************");
+		 
+		 for ( i = 0; i < max; i++){
+			 value = Math.random()*52;
+			 if(i ==0)firstmin = value;
+			 if(value > firstmin) count++;
+			 
+			 System.out.println("\t "+value);
+			 Thread.sleep(250);
+		}
+		System.out.println("\n count = "+count);
+		
+	}//end main
+}//end class
---
+++ Sumloop.java
@@ -0,0 +1,11 @@
+class Sumloop{
+  public static void main(String a[]){
+    int value = Integer.parseInt(a[0]);
+    int sum = 0;
+    for (int i = 1; i <= value; i++){
+      sum = sum + i;
+
+    }
+    System.out.println(sum);
+  }
+}
